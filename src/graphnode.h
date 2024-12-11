22409e278859c3e0afd3d47d9e5b47be794ab7b9
Min Wang <professional@minwang-ai.com>
2024-12-11 15:02:31 +0000

    move GraphEdge ownership to GraphNode

--- src/graphnode.h
+++ src/graphnode.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <memory>
 #include "chatbot.h"
 
 
@@ -16,7 +17,7 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
@@ -43,8 +44,8 @@ public:
 
     // proprietary functions
     void AddToken(std::string token); // add answers to list
-    void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
+    void AddEdgeToParentEdge(GraphEdge *edge);
+    void AddEdgeToChildEdge(std::unique_ptr<GraphEdge> edge);
 
     //// STUDENT CODE
     ////
